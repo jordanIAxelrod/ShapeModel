ccb29bd4b012595c024d23368726481218bf88f3
Jordan Axelrod <jordan.d.axelrod@gmail.com>
2022-11-07 17:34:46 -0500

    Fixed bug in the SVD and Closest point Algorithm

--- ShapeRegistration.py
+++ ShapeRegistration.py
@@ -13,9 +13,11 @@ date: 10.27.22
 import math
 
 import torch
+from scipy.spatial.transform import Rotation
 from scipy.spatial import KDTree
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 # This is the first step of the registration. We would like to make all the shapes unit length
 def normalize_shape(point_cloud: torch.Tensor):
@@ -45,7 +47,7 @@ def rotate_principal_moment_inertia(point_cloud: torch.Tensor):
     :return: The same shapes rotated to the z axis
     """
     _, _, V = torch.pca_lowrank(point_cloud)
-    theta = torch.acos(V[:, 0, 0] / torch.linalg.norm(V[:, :, 0], dim=1))
+    theta = torch.acos(V[:, 0, 2] / torch.linalg.norm(V[:, :, 0], dim=1))
 
     rotation = torch.zeros(point_cloud.shape[0], 3, 3)
 
@@ -97,8 +99,9 @@ def get_closest_points(
         weights_k = torch.gather(weights[i], -1, ii)  # (n_points)
         weight_list.append(weights_k.unsqueeze(0))
         points = torch.gather(point_cloud[i], 0,
-                              ii.unsqueeze(1).expand(n_points, 3))  # (n_points, 3d)
-        transform_points = torch.einsum('ij, nj -> ni', transformations[i][0], points) + transformations[i][1].reshape(1, 3)
+                              ii.unsqueeze(1).expand(n_points, 3)).clone()  # (n_points, 3d)
+        transform_points = torch.einsum('ij, nj -> ni', transformations[i][0], points) + transformations[i][1].reshape(
+            1, 3)
         point_list.append(transform_points.unsqueeze(0))
         distance_list.append(dd)
     med_dist = torch.median(torch.Tensor(distance_list), dim=0)[0]
@@ -112,17 +115,26 @@ def create_kd_trees(points: torch.Tensor, K: int) -> list[KDTree]:
 
 
 def weighted_mean(weights: torch.Tensor, points: torch.Tensor) -> torch.Tensor:
+
     return torch.sum(weights.reshape(-1, 1) * points, dim=0) / torch.sum(weights)
-def print_points(thing, shapes, K, title=''):
+
+
+def print_points(thing, shapes, K, title='', q=None):
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     plt.title(title)
+    if q is not None:
+        ax.scatter3D(q[:, 0], q[:, 1], q[:, 2], label='q')
+
     for i in range(K):
         transformed_shape = torch.matmul(thing['Pose Estimation'][i][0], shapes[i].T).T + thing['Pose Estimation'][i][
             1].unsqueeze(0)
-        ax.scatter3D(transformed_shape[:, 0], transformed_shape[:, 1], transformed_shape[:, 2])
+        ax.scatter3D(transformed_shape[:, 0], transformed_shape[:, 1], transformed_shape[:, 2], label=f'{i}')
+    plt.legend()
     plt.show()
-def main(points: torch.Tensor, mu: float = .1, global_max: int = 100, local_max: int = 100, lmda: float = 3):
+
+
+def main(points: torch.Tensor, mu: float = .1, global_max: int = 100, local_max: int = 100, lmda: float = 10):
     """
     Runs the main loop of the algorithm
     :param points: A tensor of K point clouds
@@ -135,44 +147,35 @@ def main(points: torch.Tensor, mu: float = .1, global_max: int = 100, local_max:
     """
     K, n_points, _ = points.shape
 
-    # Create trees in the original space
 
-    # creating a decent initial guess of the shapes
-    # points = normalize_shape(points)
-    centroids = - torch.mean(points, dim=1)
-
-    points, rotation = rotate_principal_moment_inertia(points + centroids.unsqueeze(1))
-    # initialize the first guess transformation
+    # Initialize empty transformations
     t_k = [[torch.eye(3), torch.zeros(3)] for k in range(K)]
 
+    # Create trees in the first guess space
     kdtrees = create_kd_trees(points, K)
     chi_n = torch.full((K,), np.infty)
     chi_o = torch.full((K,), np.infty)
 
-    print_points({'Pose Estimation':t_k}, points, K)
-    w_hat = torch.ones((K, n_points))
+    print_points({'Pose Estimation': t_k}, points, K, 'inital')
+    w_hat = torch.ones(K, n_points)
     global_count = 0
 
-    while (any(abs(chi_n - chi_o) >= mu * chi_o) or not global_count) and global_count < global_max:
+    while (any(-(chi_n - chi_o) >= mu * chi_o) or not global_count) and global_count < global_max:
         global_count += 1
         temp = chi_n.clone()
-        chi_n = chi_o.clone()
         chi_o = temp
-        p = []
-        w = []
-        x = []
+        p, w, x, t = [], [], [], []
         for k in range(K):
             chi_p_n = chi_o[k].clone()
             chi_p_o = chi_o[k].clone()
             local_count = 0
             points_not = points[k].clone()  # (n_points, 3d)
+            t_k_l = [t_k[k][0].clone(), t_k[k][1].clone()]
 
             w_hat_not = w_hat[k].clone()
-            while (abs(chi_p_n - chi_p_o) >= mu * chi_p_o or not local_count) and local_count < local_max:
+            while (-(chi_p_n - chi_p_o) >= mu * chi_p_o or not local_count) and local_count < local_max:
                 # update the chi's
-
                 temp = chi_p_n.clone()
-                chi_p_n = chi_p_o.clone()
                 chi_p_o = temp
 
                 # increment the local count.
@@ -182,58 +185,82 @@ def main(points: torch.Tensor, mu: float = .1, global_max: int = 100, local_max:
                 N = get_closest_points(points, k, t_k, w_hat, kdtrees)
                 N['chi_o'] = chi_o[torch.arange(K) != k].reshape(K - 1, 1).expand(-1, n_points)  # (K - 1, n_points)
                 # Get the lambda double prime
-                lmda_d_p = math.sqrt(
-                    max(lmda, math.sqrt(2) / (math.sqrt(2) - 1))
-                ) / chi_p_o * N['median']  # (K - 1, n_points)
+                lmda_d_p = math.sqrt(max(lmda, math.sqrt(2) / (math.sqrt(2) - 1))) / chi_p_o * N['median']  # (K - 1, n_points)
 
                 # whether the prime weight is within the noise envelope
                 boolean = torch.norm(points_not.unsqueeze(0) - N['points'], dim=-1) <= lmda_d_p * chi_p_o
                 # Weighted mean of the nearest points
-                w_p_m = (1 - (torch.norm(points_not.unsqueeze(0) - N['points'], dim=-1)
-                              / (lmda_d_p * chi_p_o)) ** 2) ** 2 * boolean  # (K - 1, n_points)
-                chi_min = torch.min(N['chi_o'] + (N['weights'] <= 0) * 1e20)
+                w_p_m = torch.zeros(K - 1, n_points)
+                w_p_m[boolean] = (1 - (torch.norm(points_not.unsqueeze(0) - N['points'], dim=-1)[boolean]
+                              / (lmda_d_p * chi_p_o).unsqueeze(0)
+                                       .expand(K - 1, n_points)[boolean]) ** 2) ** 2  # (K - 1, n_points)
+                print(N['weights'] <= 0)
+                chi_min = torch.min(N['chi_o'][N['weights'] > 0])
+                print(chi_min)
                 w_d_p = chi_min / N['chi_o']
-                w_bar = torch.sum(N['weights'] * w_p_m * w_d_p, dim=0).reshape(-1, 1).expand(-1, 3)
+                w_bar = torch.sum(N['weights'] * w_p_m * w_d_p, dim=0)
                 r_k_i = torch.zeros(n_points, 3)
-                r_k_i[w_bar > 0] = torch.sum((N['weights'] * w_p_m * w_d_p).unsqueeze(2) * N['points'], dim=0)[
-                                       w_bar > 0] / w_bar[w_bar > 0]
-                r_k_i[w_bar <= 0] = torch.median(N['points'], dim=0)[0][w_bar <= 0]
+
+                r_k_i[(w_bar > 0)] = torch.sum((N['weights'] * w_p_m * w_d_p)
+                                                                       .unsqueeze(2) * N['points'], dim=0)[
+                                       (w_bar > 0) & (~torch.isnan(w_bar))] / w_bar.unsqueeze(1)[(w_bar > 0) & (~torch.isnan(w_bar))]
+                r_k_i[(w_bar <= 0) | (torch.isnan(w_bar))] = torch.median(N['points'], dim=0)[0][(w_bar <= 0) | (torch.isnan(w_bar))]
                 # Consensus median centroid between the closest points, and it's self. The centroid of the points
-                q = (points_not / K + (K - 1) / K * r_k_i).squeeze(0)  # (n_points, 3d)
+                q = points_not / K + (K - 1) / K * r_k_i  # (n_points, 3d)
+                # Distance from median to its corresponding point
                 e_hat = torch.norm(q - points_not, dim=-1)  # (n_points)
                 sigma_hat = 1.5 * torch.nanmedian(e_hat)  # 1
 
+                # New weights based on the distance from the median
                 w_hat_not = (1 - (e_hat / lmda / sigma_hat) ** 2) ** 2
                 w_hat_not *= (e_hat <= lmda * sigma_hat)  # (n_points)
 
                 chi_p_n = torch.sqrt(torch.sum(w_hat_not * e_hat ** 2) / torch.sum(w_hat_not))
+                # weighted centroid of points and median shape
                 p_bar = weighted_mean(w_hat_not, points_not)
                 q_bar = weighted_mean(w_hat_not, q)
+
+                # Centered points
                 p_c = points_not - p_bar
                 q_c = q - q_bar
+                if global_count > 3:
 
-                H = torch.matmul((w_hat_not.unsqueeze(1) * p_c).T, q_c)
-                U, _, V_t = torch.svd(H)
-                R_hat = torch.matmul(V_t.t(), U.t())
+                    print(p_bar)
+                # Covariance Matrix
+                H = torch.zeros(3,3)
+                for n in range(3):
+                    for l in range(3):
+                        H[n, l] = torch.sum(w_hat_not * p_c[:, n] * q_c[:,l])
+                U, _, V = torch.svd(H)
 
+                # Rotation Matrix
+                R_hat = torch.matmul(V, U.t())
+                # Translation
                 t_hat = q_bar - torch.matmul(R_hat, p_bar.unsqueeze(1)).squeeze()
-                print(chi_p_o, chi_p_n)
+                # Transformation
                 T_hat = [R_hat, t_hat]
+                # Updated points
                 points_not = torch.matmul(T_hat[0], points_not.T).T + T_hat[1]
-                t_k[k][0] = torch.matmul(T_hat[0], t_k[k][0])
-                t_k[k][1] = torch.matmul(T_hat[0], t_k[k][1].unsqueeze(1)).squeeze() + T_hat[1]
+                # Updated Translations
+                t_k_l[0] = torch.matmul(T_hat[0], t_k_l[0])
+                t_k_l[1] = torch.matmul(T_hat[0], t_k_l[1].unsqueeze(1)).squeeze() + T_hat[1]
+                fake_points = points.clone()
+                fake_points[k] = points_not
+
             p.append(points_not.unsqueeze(0))
             w.append(w_hat_not.unsqueeze(0))
             x.append(chi_p_n.unsqueeze(0))
+            t.append(t_k_l)
         points = torch.cat(p, dim=0)
         w_hat = torch.cat(w, dim=0)
         chi_n = torch.cat(x, dim=0)
-    for k in range(K):
-        t_k[k][0] = torch.matmul(t_k[k][0], rotation[k])
-        t_k[k][1] = torch.matmul(t_k[k][0], centroids[k].unsqueeze(1)).squeeze() + t_k[k][1]
-        print(t_k[k][1])
+        t_k = t
+        print_points({'Pose Estimation': [[torch.eye(3), torch.zeros(3)] for _ in range(K)]}, points, K,
+                     'Point Cloud iteration', q)
+    print_points({'Pose Estimation': [[torch.eye(3), torch.zeros(3)] for _ in range(K)]}, points, K, 'Point Cloud End')
+
     return {'Pose Estimation': t_k, 'Membership': w_hat, 'q': q}
 
 
 if __name__ == '__main__':
-    main(torch.randn(10, 1000, 3))
+    main(torch.randn(10, 1000, 2))
--- TestShapeRegistration.py
+++ TestShapeRegistration.py
@@ -2,71 +2,85 @@ import numpy as np
 import torch
 import ShapeRegistration
 from scipy.spatial.transform import Rotation
+from scipy.cluster.vq import kmeans
+
+
 def create_random_rectangle(n_points):
     # create a unit size cube
     base_faces = [
-        (0, 2, 0, 2, 0, 0),
-        (0, 2, 0, 0, 0, 1),
-        (0, 0, 0, 2, 0, 1),
-        (0, 2, 0, 2, 1, 1),
-        (1, 1, 0, 2, 0, 1),
-        (0, 2, 1, 1, 0, 1)
+        (0, 100, 0, 100, 0, 0),
+        (0, 100, 0, 0, 0, 1),
+        (0, 0, 0, 100, 0, 1),
+        (0, 100, 0, 100, 1, 1),
+        (1, 1, 0, 100, 0, 1),
+        (0, 100, 1, 1, 0, 1)
     ]
-
 
     random_transform = Rotation.random().as_matrix()
     random_bias = np.random.randn(1, 3)
 
     shape = np.zeros((n_points, 3))
-    random_points = np.random.rand(n_points,2)
+    random_points = np.random.uniform(low = 0, high=100, size=(n_points, 2))
     for i in range(n_points):
         side = base_faces[np.random.randint(0, 6)]
         rand_comp = random_points[i]
         for j in range(3):
-            if side[2*j] == side[2*j + 1]:
+            if side[2 * j] == side[2 * j + 1]:
                 noise = np.random.randn(1) * 1e-2
-                rand_comp = np.insert(rand_comp, j, side[2 * j] + noise)
+                rand_comp = np.insert(rand_comp, j, side[2 * j] * 100 + noise)
                 shape[i] = rand_comp * 4
-
 
     random_shape = np.matmul(random_transform, shape.T).T + random_bias
 
     return random_shape
 
-def print_points(thing, shapes, K, q):
+
+def get_n_points(n, points):
+    points = kmeans(points, n)[0]
+    return points
+
+
+def print_points(thing, shapes, K, q, title=''):
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+    plt.title(title)
     if q is not None:
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
         ax.scatter3D(q[:, 0], q[:, 1], q[:, 2], label='q')
-        plt.legend()
         plt.show()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
     for i in range(K):
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
         transformed_shape = torch.matmul(thing['Pose Estimation'][i][0], shapes[i].T).T + thing['Pose Estimation'][i][
             1].unsqueeze(0)
-        ax.scatter3D(transformed_shape[:, 0], transformed_shape[:, 1], transformed_shape[:, 2], label='k')
-        plt.legend()
-        plt.show()
+        ax.scatter3D(transformed_shape[:, 0], transformed_shape[:, 1], transformed_shape[:, 2], label=f'{i}')
+    plt.legend()
+    plt.show()
 
-def TestMainLoop(n_points, K):
+
+def TestMainLoop(n_points, K, n):
     shapes = []
     for i in range(K):
-        shapes.append(create_random_rectangle(n_points))
-
+        print(i)
+        shape = create_random_rectangle(n_points)
+        shape = get_n_points(n, shape)
+        shapes.append(shape)
+    print(shapes)
     shapes = torch.Tensor(shapes)
     x = shapes.clone()
-    print_points({'Pose Estimation': [[torch.eye(3), torch.zeros(3)] for _ in range(K)]}, shapes, K, None)
+    print_points({'Pose Estimation': [[torch.eye(3), torch.zeros(3)] for _ in range(K)]}, shapes, K, None, 'Start')
+    # creating a decent initial guess of the shapes
+    # points = normalize_shape(points)
+    # centroids = - torch.mean(shapes, dim=1)
+    # shapes = shapes + centroids.unsqueeze(1)
+    # shapes, rotation = ShapeRegistration.rotate_principal_moment_inertia(shapes)
     thing = ShapeRegistration.main(shapes)
     print(x == shapes)
     q = thing['q']
-    print(q)
-    print(thing)
-    print_points(thing, shapes, K, q)
 
-if __name__ =='__main__':
+    print_points(thing, shapes, K, q, 'Finished')
+
+
+if __name__ == '__main__':
     shape = torch.Tensor(create_random_rectangle(1000)).unsqueeze(0)
     shape = ShapeRegistration.rotate_principal_moment_inertia(ShapeRegistration.normalize_shape(shape))[0].squeeze()
     import matplotlib.pyplot as plt
@@ -75,4 +89,4 @@ if __name__ =='__main__':
     ax = plt.axes(projection='3d')
     ax.scatter3D(shape[:, 0], shape[:, 1], shape[:, 2])
     plt.show()
-    TestMainLoop(1100, 10)
+    TestMainLoop(5000, 10, 500)
